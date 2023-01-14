6d245fa6415d6f2f31d30f46ee7ea3b11eb8fca8
jayruin <28711665+jayruin@users.noreply.github.com>
2023-01-14 07:35:13 -0500

    Fix workflow

--- scripts/test.py
+++ scripts/test.py
@@ -22,6 +22,7 @@ for path in projects_directory.iterdir():
     project_name = path.name
     run([
         "dotnet", "test", project_name,
+        "--configuration", "Release"
         "--no-build",
         "--logger", "trx",
         "--logger", "html"
