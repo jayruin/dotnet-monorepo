218c7158665125274a92201b28cb48d84a4780e8
jayruin <28711665+jayruin@users.noreply.github.com>
2023-02-11 07:44:19 -0500

    FileStorage Extensions

---
+++ src/FileStorage/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace FileStorage;
+
+public static class Extensions
+{
+    public static async Task CopyToAsync(this IFile source, IFile destination)
+    {
+        await using Stream sourceStream = source.OpenRead();
+        await using Stream destinationStream = destination.OpenWrite();
+        await sourceStream.CopyToAsync(destinationStream);
+    }
+
+    public static async Task CopyToAsync(this IDirectory source, IDirectory destination)
+    {
+        foreach (IFile file in source.EnumerateFiles())
+        {
+            await file.CopyToAsync(destination.FileStorage.GetFile(destination.FullPath, file.Name));
+        }
+        foreach (IDirectory directory in source.EnumerateDirectories())
+        {
+            await directory.CopyToAsync(destination.FileStorage.GetDirectory(destination.FullPath, directory.Name));
+        }
+    }
+}
