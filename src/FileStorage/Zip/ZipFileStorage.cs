64486fc75aa65b67545ee257c698f82157223b82
jayruin <28711665+jayruin@users.noreply.github.com>
2023-02-11 14:27:33 -0500

    FileStorage simplify zip constructor

--- src/FileStorage/Zip/ZipFileStorage.cs
+++ src/FileStorage/Zip/ZipFileStorage.cs
@@ -9,14 +9,9 @@ public sealed class ZipFileStorage : IFileStorage, IDisposable
 {
     internal ZipArchive Archive { get; set; }
 
-    public ZipFileStorage(Stream stream, bool read, bool write)
+    public ZipFileStorage(Stream stream)
     {
-        ZipArchiveMode mode;
-        if (read && write) mode = ZipArchiveMode.Update;
-        else if (read) mode = ZipArchiveMode.Read;
-        else if (write) mode = ZipArchiveMode.Create;
-        else throw new InvalidOperationException();
-        Archive = new ZipArchive(stream, mode, true);
+        Archive = new ZipArchive(stream, ZipArchiveMode.Update, true);
     }
 
     public IFile GetFile(params string[] paths)
