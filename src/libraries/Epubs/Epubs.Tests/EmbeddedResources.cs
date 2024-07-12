faeb3e025b9d83ee31c552ea76ead723714f8175
jayruin <28711665+jayruin@users.noreply.github.com>
2024-07-12 17:07:05 -0400

    Fix failing Epubs tests

--- src/libraries/Epubs/Epubs.Tests/EmbeddedResources.cs
+++ src/libraries/Epubs/Epubs.Tests/EmbeddedResources.cs
@@ -7,8 +7,8 @@ public static class EmbeddedResources
 {
     public static Stream? Read(string resource)
     {
-        string assemblyName = "Epub.Tests";
-        Assembly assembly = Assembly.Load(assemblyName);
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        string assemblyName = assembly.GetName().Name ?? string.Empty;
         resource = resource.Replace('/', '.').Replace('\\', '.');
         resource = string.Join('.', assemblyName, resource);
         return assembly.GetManifestResourceStream(resource);
